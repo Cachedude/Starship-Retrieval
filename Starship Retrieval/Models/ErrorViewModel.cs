ee33a7d6c096e90e8b432c54302a10e41cc5b485
caleb <caleb@DESKTOP-A6GRH38>
2023-06-03 12:14:20 -0600

    Add project files.

---
+++ Starship Retrieval/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Starship_Retrieval.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
